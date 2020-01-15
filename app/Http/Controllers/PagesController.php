71df9873728da5bc1c9ec65551cb90ccfaf81240
virak0001 <virakcambodia44@gmial.com>
2020-01-15 10:58:28 +0700

    table

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -35,8 +35,18 @@ class PagesController extends Controller
         $title = "Contact";
         return view('pages.contact') -> with('title',$title);
     }
-    public function showCustomer(){
-        $customer = array('Chandaraty','Naly','Samphas','Donat');
+    public function listCustomer(){
+        $customer = array(
+
+'0'=>array('id' => 1,'name' => 'Chandaraty','email' => 'chandaraty@example.org'),
+
+'1'=>array('id' => 2,'name' => 'Maly','email' => 'maly@example.org'),
+
+'2'=>array('id' => 3,'name' => 'Samphas','email' =>'samphas@example.org'),
+
+'3'=>array('id' => 4,'name' => 'Donat','email' => 'donat@example.org')
+
+);
         return view('pages.customer') -> with('customer',$customer);
     }
 }
